57436094a396a9892c72824626f52b98c7cc6d3f
MomciloDordevic <qwertyumom@gmail.com>
2021-08-31 19:16:23 +0000

    Finished env.py file, imported Flask and made a test to see if the return statement prints out

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 import os
-from flask import flask
+from flask import Flask
 
 if os.path.exists("env.py"):
     import env
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def testing():
+    return "Testing"
+
+# Dont forget to change to debug = False before submiting the project
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
