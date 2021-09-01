b0af5040219c2c2190b772537ec4684eab7e1747
MomciloDordevic <qwertyumom@gmail.com>
2021-09-01 19:06:09 +0000

    finished register.html, added register function in app.py ,styling now

--- app.py
+++ app.py
@@ -26,8 +26,27 @@ def get_pizzas():
     return render_template("pizzas.html", pizzas=pizzas)
 
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # Checks if the username already exists 
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # Puts the new user into 'session'
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
 # Dont forget to change to debug = False before submiting the project
