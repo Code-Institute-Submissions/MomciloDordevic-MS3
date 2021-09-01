f293dcc1c949486b8ccdd091ff1cc3fffba2ec76
MomciloDordevic <qwertyumom@gmail.com>
2021-09-01 18:08:10 +0000

    added register page urls, added new @app.route for register, added template/register.html with card template from Materialize

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -23,6 +24,11 @@ mongo = PyMongo(app)
 def get_pizzas():
     pizzas = mongo.db.pizzas.find()
     return render_template("pizzas.html", pizzas=pizzas)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 # Dont forget to change to debug = False before submiting the project
 
