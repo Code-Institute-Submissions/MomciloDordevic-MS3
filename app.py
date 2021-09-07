37e0e760f14eac262c8a1e642719b35d72054676
MomciloDordevic <qwertyumom@gmail.com>
2021-09-07 13:43:34 +0000

    completed the @app.route for deleting a category, updated add_category.html and categories.html with the correct links

--- app.py
+++ app.py
@@ -249,6 +249,13 @@ def edit_category(category_id):
     return render_template("edit_category.html", category=category)
 
 
+@app.route("/delete_category/<category_id>")
+def delete_category(category_id):
+    mongo.db.categories.remove({"_id": ObjectId(category_id)})
+    flash("Category is succesfully deleted")
+    return redirect(url_for("categories"))
+
+
 # ---- Error Handlers
 
 
